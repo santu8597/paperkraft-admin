2b4698f318315a7ba8dc1a2a0a46640918f0b54b
santu8597 <162991657+santu8597@users.noreply.github.com>
2026-03-08 03:28:08 +0530

    cors erroor

--- next.config.ts
+++ next.config.ts
@@ -18,6 +18,29 @@ const nextConfig: NextConfig = {
       sizeLimit: '10mb',
     },
   },
+  
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' data: https: blob:;
+              font-src 'self' data:;
+              connect-src * blob: data:;
+              frame-src 'self';
+              object-src 'none';
+            `.replace(/\s{2,}/g, ' ').trim()
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
